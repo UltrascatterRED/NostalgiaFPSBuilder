0df45e6eb374fa6d60a6b9ffacdb60403a37e473
UltrascatterRED <natbarton14@gmail.com>
2025-09-05 13:52:00 -0400

    Port from C to C++

--- src/compile-main.sh
+++ src/compile-main.sh
@@ -2,5 +2,5 @@
 
 errlog=gcc-errors.log
 echo "## Error Log - $(timedatectl show -P RTCTimeUSec) ##" >> $errlog
-gcc GameStructs.c Debug.c Render.c EngineMain.c -lGL -lGLU -lglut -lm -o DoomEngine 2>> $errlog && echo "Compilation Successful!"
+g++ GameStructs.cpp Debug.cpp Render.cpp EngineMain.cpp -lGL -lGLU -lglut -lm -o DoomEngine 2>> $errlog && echo "Compilation Successful!"
 exit 0
